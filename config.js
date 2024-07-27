9f3f672a510ce5f4acf483cb568e12e759b82cf1
KayaDrstabn <108328937+KayaDrstabn@users.noreply.github.com>
2024-07-27 13:38:38 +0400

    Update config.js

--- config.js
+++ config.js
@@ -6,10 +6,10 @@ module.exports = {
     token: process.env.token, // Developer portal > botunu seç > Bot sekmesindeki "TOKEN" buraya yazılıcak.
     mongo: process.env.mongo, // https://www.mongodb.com/ sitesindeki bağlantı linki buraya yazılıcak.(Veriler burada depolanıcak!)
     prefix: ".", // Bot prefixi buraya yazılıcak.
-    redirectURI: "http://10.214.205.176:4848", // Developer portal > botunu seç > OAuth2 sekmesindeki "Redirects" buraya yazılıcak. Vds kullanıyorsanız ip adresini yazıp yanına port yazıp ardından kaydedin. (Örnek: http://55.224.167.186:4848)
+    redirectURI: "https://k34-157k.onrender.com, // Developer portal > botunu seç > OAuth2 sekmesindeki "Redirects" buraya yazılıcak. Vds kullanıyorsanız ip adresini yazıp yanına port yazıp ardından kaydedin. (Örnek: http://55.224.167.186:4848)
     port: 4848, // Buraya port yazılıcak. Yukarıyla aynı olmak zorundadır. (Örnek portlar; 80 , 4848)
     owners: ["1255925204555927656" , "846255119044706334", "377527150527381504"], // Bot sahipleri buraya yazılıcak.(Birden fazla yazılabilir ve sadece buradaki kişiler botu kullanabilir!)
-    redirectionIP: ["http://10.214.205.176:4848"], // Yukarıdaki ip buraya yazılıcak. (Örnek: http://55.224.167.186:4848)
+    redirectionIP: ["https://k34-157k.onrender.com"], // Yukarıdaki ip buraya yazılıcak. (Örnek: http://55.224.167.186:4848)
     botlink: ["https://discord.com/oauth2/authorize?client_id=1255943052963614820&permissions=8&integration_type=0&scope=bot"], // Developer portal > botunu seç > OAuth2 sekmesindeki "OAuth2 URL Generator" buraya yazılıcak.
     redirectionBot: ["http://10.214.205.176:4849"],
     redirectport: 4849, // Yetkilendirme yapıldıktan sonra yönlendiriceği link. (Buraya ip adresi yazılıcak.) (Örnek: http://55.224.167.186)
